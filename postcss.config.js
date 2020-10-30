1505bfc9e0aa98d2c904ed459ebebdfdfea8f0fb
sanczol <adamfaryna@gmail.com>
2020-10-30 23:15:36 +0000

    init

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+
+module.exports = {
+  plugins: [
+    require('autoprefixer'),
+    ...process.env.HUGO_ENVIRONMENT === 'production'
+      ? [purgecss]
+      : []
+  ]
+}
