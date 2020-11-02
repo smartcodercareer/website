9f3632fcaee2e932a35ef02c869def7b4129db7e
sanczol <adamfaryna@gmail.com>
2020-11-02 22:50:01 +0000

    fixed menu and hamburger

--- assets/js/script.js
+++ assets/js/script.js
@@ -387,16 +387,15 @@
       // });
 
       // mobile menu toggle
-      // $(".menu-bar").on('click', function() {
-      //   $(this).addClass('active');
-      //   $('.main_menu').addClass('opened');
-      // });
-      // $(".close_menu").on('click', function() {
-      //   $('.menu-bar').removeClass('active');
-      //   $('.main_menu').removeClass('opened');
-      // });
+      $(".menu-bar").on('click', function() {
+        $(this).addClass('active');
+        $('.main_menu').addClass('opened');
+      });
+      $(".close_menu, .nav-link").on('click', function() {
+        $('.menu-bar').removeClass('active');
+        $('.main_menu').removeClass('opened');
+      });
     });
-
   });
 
   if (WOW) {
