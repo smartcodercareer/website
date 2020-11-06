fe23a181a3080d05c81739212fb18ff6bd854348
sanczol <adamfaryna@gmail.com>
2020-11-06 21:39:23 +0000

    Bring back animated counter

--- assets/js/script.js
+++ assets/js/script.js
@@ -142,25 +142,25 @@
     });
 
     // Counter js
-    // $(window).scroll(startCounter);
-    // function startCounter() {
-    //   var hT = jQuery('.love_counter').offset().top,
-    //       hH = jQuery('.love_counter').outerHeight(),
-    //       wH = jQuery(window).height();
-    //   if (jQuery(window).scrollTop() > hT+hH-wH) {
-    //     jQuery(window).off("scroll", startCounter);
-    //     jQuery('.love_count').each(function () {
-    //       var $this = jQuery(this);
-    //       jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
-    //         duration: 2000,
-    //         easing: 'swing',
-    //         step: function () {
-    //           $this.text(Math.ceil(this.Counter) );
-    //         }
-    //       });
-    //     });
-    //   }
-    // }
+    $(window).scroll(startCounter);
+    function startCounter() {
+      var hT = jQuery('.love_counter').offset().top,
+          hH = jQuery('.love_counter').outerHeight(),
+          wH = jQuery(window).height();
+      if (jQuery(window).scrollTop() > hT+hH-wH) {
+        jQuery(window).off("scroll", startCounter);
+        jQuery('.love_count').each(function () {
+          var $this = jQuery(this);
+          jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+            duration: 2000,
+            easing: 'swing',
+            step: function () {
+              $this.text(Math.ceil(this.Counter) );
+            }
+          });
+        });
+      }
+    }
 
     // Loader Js
     $(window).on("load", function(){
