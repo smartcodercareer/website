07f7c005f9d342cc6f88843912a22b07c9fe6fa8
sanczol <adamfaryna@gmail.com>
2020-11-08 12:29:57 +0000

    Better postCSS config for production

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,50 @@
-const purgecss = require('@fullhuman/postcss-purgecss')
-
 module.exports = {
   plugins: [
+    require('postcss-preset-env'),
     require('autoprefixer'),
+    require('postcss-reporter'),
     ...process.env.HUGO_ENVIRONMENT === 'production'
-      ? [purgecss({
-        content: ['./**/*.html', './**/*.css']
+      ? [require('@fullhuman/postcss-purgecss')({
+        content: ['./**/*.html', './**/*.css'],
+        variables: true,
+        fontFace: true,
+        keyframes: true,
+        // rejected: true,
+        // safelist: [],
+      }), require('cssnano')({
+        preset: [require('cssnano-preset-default'), {
+          discardComments: {removeAll: true},
+          colormin: true,
+          cssDeclarationSorter: true,
+          rawCache: true,
+          calc: true,
+          convertValues: true,
+          discardDuplicates: true,
+          discardEmpty: true,
+          discardOverridden: true,
+          discardUnused: true,
+          mergeIdents: true,
+          mergeLonghand: true,
+          minifyFontValues: true,
+          minifyGradients: true,
+          minifyParams: true,
+          minifySelectors: true,
+          normalizeCharset: true,
+          normalizeDisplayValues: true,
+          normalizePositions: true,
+          normalizeRepeatStyle: true,
+          normalizeString: true,
+          normalizeTimingFunctions: true,
+          normalizeUnicode: true,
+          normalizeUrl: true,
+          normalizeWhitespace: true,
+          reduceInitial: true,
+          reduceTransforms: true,
+          svgo: true,
+          uniqueSelectors: true,
+          reduceIdents: true
+        }]
       })]
     : []
   ]
-}
+};
