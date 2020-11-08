e06ed63f9fb0c5a325fa7be0640166edd3d1f7f9
sanczol <adamfaryna@gmail.com>
2020-11-08 08:40:05 +0000

    Enabled purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -3,10 +3,10 @@ const purgecss = require('@fullhuman/postcss-purgecss')
 module.exports = {
   plugins: [
     require('autoprefixer'),
-    // ...process.env.HUGO_ENVIRONMENT === 'production'
-    // ? [purgecss({
-    // content: ['./**/*.html', './**/*.css']
-    // })]
-    // : []
+    ...process.env.HUGO_ENVIRONMENT === 'production'
+      ? [purgecss({
+        content: ['./**/*.html', './**/*.css']
+      })]
+    : []
   ]
 }
