506bbb53b219f50a24369d1a6e4a35aa1e1ef08c
sanczol <adamfaryna@gmail.com>
2020-12-04 22:43:36 +0000

    moving forward

--- deploy.sh
+++ deploy.sh
@@ -33,7 +33,7 @@ echo "Checking out $branch branch into public"
 git worktree add -B $branch public $repo/$branch
 
 echo "Generating site (minified HTML)"
-hugo --minify
+hugo --minify --ignoreCache --cleanDestinationDir --gc
 
 cd public
 
