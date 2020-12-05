870291345ed28ceab9655b153ad68e5994d2eec1
sanczol <adamfaryna@gmail.com>
2020-12-05 13:54:03 +0000

    Fixed lazy loading

--- assets/js/lazy.js
+++ assets/js/lazy.js
@@ -5,15 +5,16 @@ window.onload = () => {
     var largeImage = new Image();
     largeImage.src = element.getAttribute('data-src');
     largeImage.sizes = element.hasAttribute("data-sizes") ? element.getAttribute("data-sizes") : "";
-
     largeImage.srcset = element.hasAttribute("data-srcset") ? element.getAttribute("data-srcset") : "";
     largeImage.className = element.getAttribute("class");
     largeImage.title = element.getAttribute("title");
     largeImage.alt = element.getAttribute("alt");
 
     largeImage.onload = function () {
-      largeImage.classList.add('loaded');
-      element.replaceWith(largeImage);
+      element.src = element.getAttribute('data-src');
+      element.sizes = element.hasAttribute("data-sizes") ? element.getAttribute("data-sizes") : "";
+      element.srcset = element.hasAttribute("data-srcset") ? element.getAttribute("data-srcset") : "";
+      element.classList.add('loaded');
     }
   })
 };
