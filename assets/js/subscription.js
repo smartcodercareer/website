26ba3e5d215f5651f57b7145808f5c2b538d0dc4
sanczol <adamfaryna@gmail.com>
2020-12-07 21:22:50 +0000

    More improvements

--- assets/js/subscription.js
+++ assets/js/subscription.js
@@ -10,18 +10,18 @@
 //   m.parentNode.insertBefore(a, m)
 // })(window, document, 'script', 'https://cdn.sender.net/accounts_resources/universal.js', 'sender');
 
-(function (s, e, n, d, er) {
-  s['Sender'] = er
-  s[er] = s[er] || function () {
-    (s[er].q = s[er].q || []).push(arguments)
-  }, s[er].l = 1 * new Date();
-  var a = e.createElement(n);
-  // m = e.getElementsByTagName(n)[0];
-  a.async = 1;
-  a.src = d;
-  // m.parentNode.insertBefore(a, m)
-  document.head.append(a);
-})(window, document, 'script', 'https://cdn.sender.net/accounts_resources/universal.js', 'sender');
+// (function (s, e, n, d, er) {
+//   s['Sender'] = er
+//   s[er] = s[er] || function () {
+//     (s[er].q = s[er].q || []).push(arguments)
+//   }, s[er].l = 1 * new Date();
+//   var a = e.createElement(n);
+//   // m = e.getElementsByTagName(n)[0];
+//   a.async = 1;
+//   a.src = d;
+//   // m.parentNode.insertBefore(a, m)
+//   document.head.append(a);
+// })(window, document, 'script', 'https://cdn.sender.net/accounts_resources/universal.js', 'sender');
 
 // show popup: Ebook: Learn 12 Reasons Why People Fail To Learn to Code And How To Overcome Them overview
 sender('b2aa78d27b1267');
