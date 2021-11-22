0bd5785021beb74e3bb493b2f7112bddbeb8850f
farynaio <adam22@lifeisgood.cyou>
2021-11-22 23:40:16 +0000

    Fixed release build

--- deploy.sh
+++ deploy.sh
@@ -66,6 +66,6 @@ fi
 # echo $includes >> _config.yml
 
 echo "Updating $branch branch"
-git add --all && git commit --allow-empty -m "Publishing to $branch" && git push -f $repo $branch
+git add --all && git commit --allow-empty -m "Publishing to $branch" && git push -f $repo master:$branch
 
 cd ..
